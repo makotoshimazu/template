9dd1e29c709adf036899bd0e449a00251c5fd251
Shimazu Makoto <smk.hhs@gmail.com>
2014-01-21 11:30:06 +0900

    compgen option is modified

--- completion.sh
+++ completion.sh
@@ -7,9 +7,7 @@ _template()
     unalias ls
     case "${COMP_CWORD}" in
         "1")
-            COMPREPLY=( $(\
-compgen -W "`ls ${TEMPLATE_DIR}`" \
-                ${COMP_WORDS[COMP_CWORD]}) )
+            COMPREPLY=( $(compgen -C "ls ${TEMPLATE_DIR}" ${COMP_WORDS[COMP_CWORD]}) )
             ;;
     esac
 }
