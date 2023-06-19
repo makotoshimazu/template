33864f96c7b317a062a39f445c0320c2fa9e449c
Makoto Shimazu <makoto.shimaz@gmail.com>
2023-06-19 21:07:54 +0900

    updated

--- completion.sh
+++ completion.sh
@@ -6,8 +6,8 @@ _template()
 {
     case "${COMP_CWORD}" in
         "1")
-            targets=$(find $TEMPLATE_DIR -maxdepth 1 -mindepth 1 -type d)
-            COMPREPLY=($(compgen -W "${targets}" "${COMP_WORDS[COMP_CWORD]}"))
+            targets=$(find $TEMPLATE_DIR -maxdepth 1 -mindepth 1 -type d | sed "s%${TEMPLATE_DIR}/%%" | tr "\n" " ")
+            COMPREPLY=($(compgen -W "${targets}" -- "${COMP_WORDS[COMP_CWORD]}"))
             ;;
     esac
 }
